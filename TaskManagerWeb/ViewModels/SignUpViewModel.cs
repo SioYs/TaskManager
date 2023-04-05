825515f4006870156ea55d9b6f33407dca0125d1
Angel <romfinsh@gmail.com>
2023-04-05 17:03:28 +0300

    App ViewModels

---
+++ TaskManagerWeb/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagerWeb.ViewModels
+{
+    public class SignUpViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+}
---
+++ TaskManagerWeb/ViewModels/TaskViewModel.cs
@@ -0,0 +1,10 @@
+﻿using TaskManager.Data.Models.Enums;
+
+public class TaskViewModel
+{
+    public int Id { get; set; } 
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime DueDate { get; set; }
+    public Status Status { get; set; }
+}
